1c584d6668fb969cb7eabdfceac2931536b290c0
zhangsanshi <zhangsanshi5566@gmail.com>
2020-12-08 15:04:33 +0800

    feat: add form component

--- src/components/u-flowable-form.vue/docs/blocks.md
+++ src/components/u-flowable-form.vue/docs/blocks.md
@@ -1,17 +1,17 @@
 ### 编辑模式
 
 ``` html
-	<u-flowable-form>
-			<u-flowable-form-item mode="edit" title="邮箱" tip="请输入正确邮箱">
-					<u-flowable-email  value="test@test.com" size="huge"
-							name="e.f"
-					:clearable="true" style="color:red"></u-flowable-email>
-			</u-flowable-form-item>
-			<u-flowable-form-item mode="edit" title="邮箱" tip="请输入正确邮箱">
-					<u-flowable-email value="test@test.com" size="huge" 
-					name="c.d[1]"
-					:clearable="true" 
-					style="color:red"></u-flowable-email>
-			</u-flowable-form-item>
-	</u-flowable-form>
+<u-flowable-form title="表单" tip="测试" :files="[{name: 'xxx', url: 'test.com'}]" buttonText="继续">
+	<u-flowable-form-item mode="edit" title="邮箱" tip="请输入正确邮箱">
+			<u-flowable-email  value="test@test.com" size="huge"
+					name="e.f"
+			:clearable="true" style="color:red"></u-flowable-email>
+	</u-flowable-form-item>
+	<u-flowable-form-item mode="edit" title="邮箱" tip="请输入正确邮箱">
+			<u-flowable-email value="test@test.com" size="huge" 
+			name="c.d[1]"
+			:clearable="true" 
+			style="color:red"></u-flowable-email>
+	</u-flowable-form-item>
+</u-flowable-form>
 ```
--- src/components/u-flowable-form.vue/index.vue
+++ src/components/u-flowable-form.vue/index.vue
@@ -1,12 +1,55 @@
+<template>
+    <div :class="$style.root">
+        <div :class="$style.head">
+            <div :class="$style.title">
+                {{ title }}
+            </div>
+            <div :class="$style.tip" v-if="tip">
+                {{ tip }}
+            </div>
+            <div :class="$style.files" v-if="files && files.length">
+                <div :class="$style.file" v-for="(file, index) in files" :key="index">
+                    <div :class="$style.meta">
+                        {{ file.name }}
+                    </div>
+                    <div :class="$style.action">
+                        <a download :href="file.url" target="_blank">下载文件</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div :class="$style.content">
+            <form ref="form">
+                <slot></slot>
+            </form>
+        </div>
+        <div :class="$style.foot">
+            <u-button @click="onSubmit()">{{ buttonText }}</u-button>
+        </div>
+    </div>
+</template>
 <script>
+import UButton from 'cloud-ui.vusion/src/components/u-button.vue/index.vue';
+import ULink from 'cloud-ui.vusion/src/components/u-link.vue/index.vue';
 import setPath from 'lodash/set';
 
 const UFlowableForm = {
+    components: {
+        UButton,
+        ULink,
+    },
     name: 'u-flowable-form',
     props: {
-        title: String,
+        title: {
+            type: String,
+            default: '表单',
+        },
         tip: String,
-        mode: String,
+        files: Array,
+        buttonText: {
+            type: String,
+            default: '提交',
+        },
     },
     data() {
         return {
@@ -33,19 +76,18 @@ const UFlowableForm = {
             return vueInstance;
         },
         onSubmit(event) {
-            event.preventDefault();
-            console.log(this.$el, 999);
-            console.log(this.$el.querySelectorAll('[name]'));
-            const formItems = this.$el.querySelectorAll('[name]');
+            console.log(this.$refs.form.querySelectorAll('[name]'));
+            const formItems = this.$refs.form.querySelectorAll('[name]');
             const map = {};
             const result = {};
-            const errors = {};
-            [...formItems].every((item) => {
+            let error = null;
+            const valid = [...formItems].every((item) => {
                 const name = item.getAttribute('name');
                 if (!map[name]) {
                     map[name] = true;
                     const formItem = this.getNearVueInstance(item);
                     if (formItem.$error) {
+                        error = formItem.$error;
                         item.scrollIntoView(false);
                         return false;
                     }
@@ -53,24 +95,13 @@ const UFlowableForm = {
                 }
                 return true;
             });
-            console.info('result abc', result);
+            if (valid) {
+                this.$emit('submit', result);
+            } else {
+                this.$emit('error', error);
+            }
+            console.info('result abc', valid, result);
         },
-    },
-    render(h, context) {
-        return h('form', {
-            class: this.$style.root,
-            on: {
-                submit: this.onSubmit,
-            },
-        }, [
-            this.$slots.head,
-            this.$slots.default,
-            this.$slots.foot || h('div', {
-                class: this.$style.foot,
-            }, [
-                h('button', ['提交']),
-            ]),
-        ]);
     },
 };
 
@@ -83,5 +114,48 @@ export default UFlowableForm;
 </script>
 <style module>
 .root {
+}
+.head {
+    margin-bottom: 12px;
+    box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+    background: #fff;
+    border-radius: 10px;
+}
+.title {
+    padding: 20px;
+    font-size: 18px;
+    font-weight: 500;
+}
+.tip {
+    padding: 10px 20px;
+}
+.content {
+    box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+    background: #fff;
+    border-radius: 10px;
+    padding: 12px;
+}
+.foot {
+    position: relative;
+    padding: 14px 0;
+    margin-top: 12px;
+    background: #fff;
+    text-align: center;
+    box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+    border-radius: 10px;
+}
+.files {
+        padding: 10px 20px;
+}
+.file {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10px;
+    height: 40px;
+    background-color: #f5f5f7;
+    border-radius: 3px;
+    margin-bottom: 4px;
+    cursor: pointer;
 }
 </style>
